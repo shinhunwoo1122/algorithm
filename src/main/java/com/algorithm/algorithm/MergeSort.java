9aabc3f1577db3c90ad522fd3ffd5350b5b1eef8
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2024-06-04 23:33:58 +0900

    -구간합 알고리즘 구현 -버블정렬 알고리즘 구현 -선택정렬 알고리즘 구현 -합병정렬 알고리즘 구현

--- src/main/java/com/algorithm/algorithm/MergeSort.java
+++ src/main/java/com/algorithm/algorithm/MergeSort.java
@@ -19,7 +19,6 @@ public class MergeSort {
         }
     }
 
-
     private static void merge(int[] arr, int[] tmp, int start, int mid, int end) {
 
         System.out.println("start = " + start);
--- src/main/java/com/algorithm/algorithm/SelectionSort.java
+++ src/main/java/com/algorithm/algorithm/SelectionSort.java
@@ -23,12 +23,6 @@ public class SelectionSort {
         selectionSort(arr, 0);
     }
 
-
-
-
-
-
-
     private static void selectionSort(int[] arr, int start){
         if(start < arr.length -1){
             int min_index = start;
@@ -39,9 +33,6 @@ public class SelectionSort {
             selectionSort(arr, start + 1);
         }
     }
-
-
-
 
     private static void swap(int[] arr, int index1, int index2){
         int tmp = arr[index1];
