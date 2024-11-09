d00e86d1a300bbb70776cf8898d163445312109f
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2024-11-09 12:37:11 +0900

    -구간합 알고리즘 구현 -버블정렬 알고리즘 구현 -선택정렬 알고리즘 구현 -합병정렬 알고리즘 구현

--- src/main/java/com/algorithm/algorithm/BubbleSort.java
+++ src/main/java/com/algorithm/algorithm/BubbleSort.java
@@ -21,6 +21,10 @@ public class BubbleSort {
     }
 
     private static void bubleSort(int[] arr, int last){
+
+
+
+
         if(last > 0){
             for(int i = 1; i <= last; i++){
                 //선택된 인덱스의값이 해당 arr의 값보다 크면 한칸 옆으로 이동하기 위해 swap함
@@ -43,9 +47,6 @@ public class BubbleSort {
 
     public static void swap(int[] arr, int source, int target) {
 
-
-
-
         //swap을 위한 값 source 한칸 오른쪽으로 밀기전의 값 target 보다 큰 수
         //tmp를 만들어 해당 값을 미리 지정해놓고 target에다 큰 값을 넣어준 후고 sorce값에 기존에 지정해놓은 tmp값을 넣어줘서 정렬처리함.
         int tmp = arr[source];
@@ -55,12 +56,17 @@ public class BubbleSort {
 
     private static void printArray(int[] arr){
 
+
+
         for(int data : arr){
             System.out.print(data + ", ");
         }
         System.out.println();
     }
     public static void main(String[] args) {
+
+
+
         int[] arr = {3,5,4,2,1};
         printArray(arr);
         bubleSort(arr);
